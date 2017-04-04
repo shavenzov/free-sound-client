f21d013714caf4a5b28045278b75a417a53b3bf1
Shavenzov Denis <d.shavenzov@t1-group.ru>
2017-04-04 12:29:47 +0300

    Auto-generated commit

--- out-tsc/app/pipes/file-size.pipe.spec.js
+++
@@ -1,8 +0,0 @@
-import { FileSizePipe } from './file-size.pipe';
-describe('FileSizePipe', function () {
-    it('create an instance', function () {
-        var pipe = new FileSizePipe();
-        expect(pipe).toBeTruthy();
-    });
-});
-//# sourceMappingURL=F:/server/projects/Angular-2/free-sound-client/src/app/pipes/file-size.pipe.spec.js.map
--- out-tsc/app/pipes/value-description.pipe.spec.js
+++
@@ -1,8 +0,0 @@
-import { ValueDescriptionPipe } from './value-description.pipe';
-describe('ValueDescriptionPipe', function () {
-    it('create an instance', function () {
-        var pipe = new ValueDescriptionPipe();
-        expect(pipe).toBeTruthy();
-    });
-});
-//# sourceMappingURL=F:/server/projects/Angular-2/free-sound-client/src/app/pipes/value-description.pipe.spec.js.map
